cf584e3593b267f090a8b70126e52364f0dd8c27
luan-brav0 <luan.melo.bravo10@gmail.com>
2023-02-06 10:46:31 -0300

    theprimeagen vimrc in progress

---
+++ lua/lul/init.lua
@@ -0,0 +1,4 @@
+require("lul.remap") 
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
---
+++ lua/lul/packer.lua
@@ -0,0 +1,13 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use { "ellisonleao/gruvbox.nvim" }
+  -- use 'nvim-lua/plenary.nvim'
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  requires = {{ 'nvim-lua/plenary.nvim' }}
+ }
+  use ('nvim-treesitter/nvim-treesitter', { run =':TSUpdate'})
+end)
