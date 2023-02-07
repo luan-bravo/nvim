67f2cced3f44253b32d2a999bb97ba865e8af3a1
luan-brav0 <luan.melo.bravo10@gmail.com>
2023-02-07 11:20:02 -0300

    harpoon and undotree installed

--- lua/lul/remap.lua
+++ lua/lul/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "<leader>j", "<Esc>")
+vim.keymap.set("i", "<leader>jj", "<Esc>")
+vim.keymap.set("n", "<C-s>", vim.cmd.w)
