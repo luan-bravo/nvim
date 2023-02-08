d6d3b63c98a36effe2db601a340f186732bfe598
luan-brav0 <luan.melo.bravo10@gmail.com>
2023-02-08 13:27:09 -0300

    null-ls added

---
+++ after/plugin/null-ls.lua
@@ -0,0 +1,17 @@
+local status, null_ls = pcall(require, "null-ls")
+if not status then return end
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+    debug = false,
+    sources = {
+        formatting.prettier,
+        -- formatting.black.with({ extra_args = { "--fast " } }),
+        -- diagnostics.flake8,
+        formatting.stylua,
+        diagnostics.eslint,
+        null_ls.builtins.completion.spell
+    },
+})
--- lua/lul/packer.lua
+++ lua/lul/packer.lua
@@ -1,48 +1,50 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use { "ellisonleao/gruvbox.nvim" }
-  -- use 'nvim-lua/plenary.nvim'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  requires = {{ 'nvim-lua/plenary.nvim' }}
- }
-  use ('nvim-treesitter/nvim-treesitter', { run =':TSUpdate'})
-  use 'nvim-lua/plenary.nvim'
-  use 'ThePrimeagen/harpoon'
-  use 'mbbill/undotree'
-  use 'tpope/vim-fugitive'
-  use 'neovim/nvim-lspconfig'
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {'williamboman/mason.nvim'},           -- Optional
-		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+return require('packer').startup(
+        function(use)
+            -- Packer can manage itself
+            use 'wbthomason/packer.nvim'
+            use { "ellisonleao/gruvbox.nvim" }
+            -- use 'nvim-lua/plenary.nvim'
+            use {
+                'nvim-telescope/telescope.nvim', tag = '0.1.1',
+                requires = { { 'nvim-lua/plenary.nvim' } }
+            }
+            use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+            use 'nvim-lua/plenary.nvim'
+            use 'ThePrimeagen/harpoon'
+            use 'mbbill/undotree'
+            use 'tpope/vim-fugitive'
+            use 'neovim/nvim-lspconfig'
+            use {
+                'VonHeikemen/lsp-zero.nvim',
+                branch = 'v1.x',
+                requires = {
+                    -- LSP Support
+                    { 'neovim/nvim-lspconfig' }, -- Required
+                    { 'williamboman/mason.nvim' }, -- Optional
+                    { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},         -- Required
-		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		  {'hrsh7th/cmp-buffer'},       -- Optional
-		  {'hrsh7th/cmp-path'},         -- Optional
-		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+                    -- Autocompletion
+                    { 'hrsh7th/nvim-cmp' }, -- Required
+                    { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+                    { 'hrsh7th/cmp-buffer' }, -- Optional
+                    { 'hrsh7th/cmp-path' }, -- Optional
+                    { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+                    { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},             -- Required
-		  {'rafamadriz/friendly-snippets'}, -- Optional
-	  }
-  }
-  use 'windwp/nvim-autopairs'
-  use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use 'glepnir/lspsaga.nvim'
-  use 'cohama/lexima.vim'
-  use 'tpope/vim-rhubarb'
-end)
+                    -- Snippets
+                    { 'L3MON4D3/LuaSnip' }, -- Required
+                    { 'rafamadriz/friendly-snippets' }, -- Optional
+                }
+            }
+            use 'windwp/nvim-autopairs'
+            use {
+                'nvim-lualine/lualine.nvim',
+                requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+            }
+            use 'glepnir/lspsaga.nvim'
+            use 'cohama/lexima.vim'
+            use 'tpope/vim-rhubarb'
+            use 'jose-elias-alvarez/null-ls.nvim'
+        end)
