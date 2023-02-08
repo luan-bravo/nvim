91a9e1fa7e9fecc8be8a9d0b0c75e4d2a595f53d
luan-brav0 <luan.melo.bravo10@gmail.com>
2023-02-08 08:28:45 -0300

    lualine added

--- lua/lul/packer.lua
+++ lua/lul/packer.lua
@@ -37,4 +37,8 @@ return require('packer').startup(function(use)
 	  }
   }
   use 'windwp/nvim-autopairs'
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 end)
--- lua/lul/remap.lua
+++ lua/lul/remap.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "<leader>jj", "<Esc>")
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
+vim.keymap.set("n", "<C-a>", 'gg<S-v>G')
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
