b44d16adaa4c09b78a1e9e682c8c217d151ae891
luan-brav0 <luan.melo.bravo10@gmail.com>
2023-02-08 14:11:31 -0300

    format on save added w/ null-ls

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -1,6 +1,6 @@
-local saga = require('lspsaga')
+local saga = require("lspsaga")
 
-vim.keymap.set('n', '<C-j>', ':Lspsaga diagnostic_jump_next<CR>', { silent = true })
-vim.keymap.set('n', 'K', ':Lspsaga hover_doc<CR>', { silent = true })
-vim.keymap.set('i', '<C-k>', ':Lspsaga signature_help<CR>', { silent = true })
-vim.keymap.set('n', 'gh', ':Lspsaga lsp_finder<CR>', { silent = true })
+vim.keymap.set("n", "<C-j>", ":Lspsaga diagnostic_jump_next<CR>", { silent = true })
+vim.keymap.set("n", "K", ":Lspsaga hover_doc<CR>", { silent = true })
+vim.keymap.set("i", "<C-k>", ":Lspsaga signature_help<CR>", { silent = true })
+vim.keymap.set("n", "gh", ":Lspsaga lsp_finder<CR>", { silent = true })
