6c602efe44ae64272fe325969daee6d3724deed6
lul <luan.melo.bravo10@gmail.com>
2023-04-25 08:31:28 -0300

    text wrap enabled

--- lua/lul/set.lua
+++ lua/lul/set.lua
@@ -6,12 +6,9 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.cmd.set("wrap")
-
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
-
+vim.opt.wrap = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 -- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
