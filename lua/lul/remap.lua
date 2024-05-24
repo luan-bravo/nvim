98290a74659507037c585df50670255af7234459
luan-brav0 <luan-brav0@github.com>
2024-05-24 12:41:17 -0300

    one keymap removed | will redo nvimrc from scratch soon

--- lua/lul/remap.lua
+++ lua/lul/remap.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "<leader>jj", "<Esc>")
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
 vim.keymap.set("n", "<C-a>", "gg<S-v>G")
 vim.keymap.set("i", "<C-BS>", "<C-w>")
