4918c6e8f186686ce81608c12a9f5e4ecca36554
luan-brav0 <luan.melo.bravo10@gmail.com>
2024-09-21 18:25:40 -0300

    sync

---
+++ lua/theprimeagen/lazy/codeium.lua
@@ -0,0 +1,18 @@
+return {
+    "Exafunction/codeium.vim",
+
+    config = function()
+        vim.keymap.set("i", "<C-q>", function()
+            return vim.fn["codeium#Accept"]()
+        end, { expr = true })
+        vim.keymap.set("i", "<C-d>", function()
+            return vim.fn["codeium#CycleCompletions"](5)
+        end, { expr = true })
+        vim.keymap.set("i", "<C-f>", function()
+            return vim.fn["codeium#CycleCompletions"](3)
+        end, { expr = true })
+        vim.keymap.set("i", "<C-x>", function()
+            return vim.fn["codeium#Clear"]()
+        end, { expr = true })
+    end
+}
