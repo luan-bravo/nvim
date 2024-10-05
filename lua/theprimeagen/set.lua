0638063a11e778cd4ff9fdb432e806bbdf8169f8
luan-brav0 <luan.melo.bravo10@gmail.com>
2024-10-05 18:42:58 -0300

    syncing | in the middle of trying to figure debugging out again

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -29,3 +29,9 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+--[[
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
+]]--
