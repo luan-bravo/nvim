5985dd7747798e75f48638804a64a532be64ad62
luan-brav0 <luan-brav0@github.com>
2025-03-15 15:03:04 -0300

    harpoon now using leader,number

--- lua/theprimeagen/lazy/harpoon.lua
+++ lua/theprimeagen/lazy/harpoon.lua
@@ -10,14 +10,14 @@ return  {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader><C-1>", function() harpoon:list():replace_at(1) end)
-        vim.keymap.set("n", "<leader><C-2>", function() harpoon:list():replace_at(2) end)
-        vim.keymap.set("n", "<leader><C-3>", function() harpoon:list():replace_at(3) end)
-        vim.keymap.set("n", "<leader><C-4>", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<leader>!", function() harpoon:list():replace_at(1) end)
+        vim.keymap.set("n", "<leader>@", function() harpoon:list():replace_at(2) end)
+        vim.keymap.set("n", "<leader>#", function() harpoon:list():replace_at(3) end)
+        vim.keymap.set("n", "<leader>$", function() harpoon:list():replace_at(4) end)
     end,
 }
 
