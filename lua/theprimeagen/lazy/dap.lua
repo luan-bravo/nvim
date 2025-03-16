497ff49fac614fdcd62c036401f55c94119d1d93
luan-brav0 <luan-brav0@github.com>
2025-03-16 14:24:19 -0300

    C dap & dapui done right this time. Commented unused unconfigured daps

--- lua/theprimeagen/lazy/dap.lua
+++ lua/theprimeagen/lazy/dap.lua
@@ -4,36 +4,59 @@ return {
         "rcarriga/nvim-dap-ui",
         "theHamsta/nvim-dap-virtual-text",
         "nvim-telescope/telescope-dap.nvim",
+        "williamboman/mason.nvim",
+        "jay-babu/mason-nvim-dap.nvim",
     },
     config = function()
-        local dap = require("dap")
-
-        vim.keymap.set("n", "<F5>", require("dap").continue)
-        vim.keymap.set("n", "<F10>", require("dap").step_over)
-        vim.keymap.set("n", "<F11>", require("dap").step_into)
-        vim.keymap.set("n", "<F12>", require("dap").step_out)
-        vim.keymap.set("n", "<leader>b", require("dap").toggle_breakpoint)
-        vim.keymap.set("n", "<leader>B", function()
-            dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-        end)
-        -- Custom breakpoint sign
-        vim.fn.sign_define("DapBreakpoint", {
-            text = "🔴",
-            texthl = "SignColumn",
-            linehl = "",
-            numhl = ""
+        require("mason").setup()
+        require("mason-nvim-dap").setup({
+            ensure_installed = {
+                "codelldb",
+            },
+            automatic_setup = true,
         })
 
-        dap.adapters.c = {
-            type = "executable",
-            command = "codelldb",
-            name = "lldb"
+        local dap = require("dap")
+        local dapui = require("dapui")
+
+        -- UI
+        dapui.setup()
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+
+
+
+        local mason_registry = require("mason-registry")
+        local codelldb = mason_registry.get_package("codelldb")
+        local codelldb_path = codelldb:get_install_path() .. "/extension/adapter/codelldb"
+
+        -- Custom breakpoint sign
+
+        dap.adapters.codelldb = {
+            type = "server",
+            port = "${port}",
+            executable = {
+                command = codelldb_path,
+                args = {
+                    "--port", "${port}",
+                }
+            }
         }
 
         dap.configurations.c = {
             {
                 name = "Launch file",
-                type = "c",
+                type = "codelldb",
                 request = "launch",
                 program = function()
                     return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
@@ -43,9 +66,19 @@ return {
                 args = {},
             },
         }
+
+        --[[
+        dap.adapters.c = {
+            type = "executable",
+            command = "codelldb",
+            name = "lldb"
+        }
+        ]]
+
+        -- TODO: Actually set these up later and for js/ts
+        --[[
         dap.configurations.cpp = dap.configurations.c
         dap.configurations.zig = dap.configurations.c
-
         dap.adapters.cpp = dap.adapters.c
         dap.adapters.zig = dap.adapters.c
         dap.adapters.rust = dap.adapters.c
@@ -62,7 +95,6 @@ return {
                 args = {},
             },
         }
-
         dap.adapters.go = {
             type = "server",
             port = "${port}",
@@ -71,7 +103,6 @@ return {
                 args = { "dap", "-l", "127.0.0.1:${port}" },
             }
         }
-
         dap.configurations.go = {
             {
                 type = "go",
@@ -87,6 +118,38 @@ return {
                 program = "${file}",
             },
         }
+        ]]
+
+        vim.fn.sign_define("DapBreakpoint", {
+            text = "🔴",
+            texthl = "SignColumn",
+            linehl = "",
+            numhl = ""
+        })
+
+        -- Steps
+        vim.keymap.set("n", "<F1>", dap.continue)
+        vim.keymap.set("n", "<F2>", dap.step_into)
+        vim.keymap.set("n", "<F3>", dap.step_over)
+        vim.keymap.set("n", "<F4>", dap.step_out)
+        vim.keymap.set("n", "<F5>", dap.step_back)
+        -- Breakpoints
+        vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+        vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
+        vim.keymap.set("n", "<leader>B", function()
+            dap.clear_breakpoints()
+        end)
+        vim.keymap.set("n", "<leader>%", function()
+            dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+        end)
+        vim.keymap.set("n", "<leader>?", function()
+            -- Eval val under cursor (taken from tjdevries)
+            dapui.eval(nil, { enter = true })
+        end)
+        -- Debug session
+        vim.keymap.set("n", "<leader>*", dap.terminate)
+        vim.keymap.set("n", "<leader>&", dap.restart)
+
 
         -- Telescope integration
         require("telescope").load_extension("dap")
