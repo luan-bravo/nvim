d3f59f2aa8701159086f3f8f1ade43ed39d872c1
luan-brav0 <luan-brav0@github.com>
2025-03-17 10:57:42 -0300

    dap now creates virtual text, its keymaps now also work while on insert mode and fixed terminate and restart keymaps

--- lua/theprimeagen/lazy/dap.lua
+++ lua/theprimeagen/lazy/dap.lua
@@ -18,6 +18,22 @@ return {
 
         local dap = require("dap")
         local dapui = require("dapui")
+        require("nvim-dap-virtual-text").setup({
+            -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+            display_callback = function(variable)
+              local name = string.lower(variable.name)
+              local value = string.lower(variable.value)
+              if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
+                return "*****"
+              end
+
+              if #variable.value > 15 then
+                return " " .. string.sub(variable.value, 1, 15) .. "... "
+              end
+
+              return " " .. variable.value
+            end,
+      })
 
         -- UI
         dapui.setup()
@@ -128,11 +144,11 @@ return {
         })
 
         -- Steps
-        vim.keymap.set("n", "<F1>", dap.continue)
-        vim.keymap.set("n", "<F2>", dap.step_into)
-        vim.keymap.set("n", "<F3>", dap.step_over)
-        vim.keymap.set("n", "<F4>", dap.step_out)
-        vim.keymap.set("n", "<F5>", dap.step_back)
+        vim.keymap.set({ "n", "i" }, "<F1>", dap.continue)
+        vim.keymap.set({ "n", "i" }, "<F2>", dap.step_into)
+        vim.keymap.set({ "n", "i" }, "<F3>", dap.step_over)
+        vim.keymap.set({ "n", "i" }, "<F4>", dap.step_out)
+        vim.keymap.set({ "n", "i" }, "<F5>", dap.step_back)
         -- Breakpoints
         vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
         vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
@@ -147,8 +163,12 @@ return {
             dapui.eval(nil, { enter = true })
         end)
         -- Debug session
-        vim.keymap.set("n", "<leader>*", dap.terminate)
-        vim.keymap.set("n", "<leader>&", dap.restart)
+        vim.keymap.set("n", "<leader>*", function()
+            dap.terminate()
+        end)
+        vim.keymap.set("n", "<leader>&", function()
+            dap.restart()
+        end)
 
 
         -- Telescope integration
