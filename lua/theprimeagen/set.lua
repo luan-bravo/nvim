4fc60f2f814cac73355b1d350924a4406f7d1edf
luan-brav0 <luan-brav0@github.com>
2025-04-21 02:54:07 -0300

    Mod: Change white space characters

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -29,9 +29,8 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "80"
 
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+vim.opt.listchars:append("space:⠂") -- \uec07
+vim.opt.listchars:append("eol:") -- \ue621
