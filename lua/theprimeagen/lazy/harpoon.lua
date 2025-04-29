e859a854243a7212daf54e1f12c9baf6ce145056
luan-brav0 <luan-brav0@github.com>
2025-04-29 20:01:58 -0300

    Add: description to harpoon keymaps

--- lua/theprimeagen/lazy/harpoon.lua
+++ lua/theprimeagen/lazy/harpoon.lua
@@ -4,20 +4,17 @@ return  {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local harpoon = require("harpoon")
-
         harpoon:setup()
-
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader>!", function() harpoon:list():replace_at(1) end)
-        vim.keymap.set("n", "<leader>@", function() harpoon:list():replace_at(2) end)
-        vim.keymap.set("n", "<leader>#", function() harpoon:list():replace_at(3) end)
-        vim.keymap.set("n", "<leader>$", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon add" })
+        vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon toggle menu" })
+        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon 1" })
+        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon 2" })
+        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon 3" })
+        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon 4" })
+        vim.keymap.set("n", "<leader>!", function() harpoon:list():replace_at(1) end, { desc = "Harpoon 1" })
+        vim.keymap.set("n", "<leader>@", function() harpoon:list():replace_at(2) end, { desc = "Harpoon 2" })
+        vim.keymap.set("n", "<leader>#", function() harpoon:list():replace_at(3) end, { desc = "Harpoon 3" })
+        vim.keymap.set("n", "<leader>$", function() harpoon:list():replace_at(4) end, { desc = "Harpoon 4" })
     end,
 }
 
