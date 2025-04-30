7531877698b6ed5e786a31d121e994a752c6bdba
luan-brav0 <luan-brav0@github.com>
2025-04-30 20:51:11 -0300

    Minor: formating user commands

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -106,29 +106,29 @@ vim.filetype.add({
 -- Access nvim config anywhere in a new tab
 vim.api.nvim_create_user_command('Configuration', function()
     -- Create a new tab
-        vim.cmd('tabnew')
-        -- Change directory for the current tab to ~/.config/nvim
-        vim.cmd('tcd ~/.config/nvim')
-        -- Open the file explorer (using Netrw)
-        vim.cmd('Explore')
-    end,
-{ desc = 'Open nvim config dir in a new tab with Explorer' }
-)
+    vim.cmd('tabnew')
+    -- Change directory for the current tab to ~/.config/nvim
+    vim.cmd('tcd ~/.config/nvim')
+    -- Open the file explorer (using Netrw)
+    vim.cmd('Explore')
+end, { desc = 'Open nvim config dir in a new tab with Explorer' })
 
 -- Access notes dir anywhere in a new tab
 vim.api.nvim_create_user_command('Notes', function()
     -- Create a new tab
-        vim.cmd('tabnew')
-        -- Change directory for the current tab to ~/.config/nvim
-        vim.cmd('tcd ~/notes')
-        -- Open the file explorer (using Netrw)
-        vim.cmd('Explore')
-    end,
-{ desc = 'Open nvim config dir in a new tab with Explorer' }
-)
+    vim.cmd('tabnew')
+    -- Change directory for the current tab to ~/.config/nvim
+    vim.cmd('tcd ~/notes')
+    -- Open the file explorer (using Netrw)
+    vim.cmd('Explore')
+end, { desc = 'Open nvim config dir in a new tab with Explorer' })
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 0
+
+vim.api.nvim_create_user_command('Format', function()
+    vim.lsp.buf.format()
+end, { desc = 'Open nvim config dir in a new tab with Explorer' })
