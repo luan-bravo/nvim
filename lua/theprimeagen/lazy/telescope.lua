e1c2173066dbb828b641ccd63a15000cf11b8125
luan-brav0 <luan-brav0@github.com>
2025-05-01 00:29:59 -0300

    Add: more telescope keymaps for current buffer, live grep, registers, keymaps...

--- lua/theprimeagen/lazy/telescope.lua
+++ lua/theprimeagen/lazy/telescope.lua
@@ -9,22 +9,39 @@ return {
 
     config = function()
         require('telescope').setup({})
+        local builtin = require("telescope.builtin")
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<C-p>', builtin.find_files, {}, { desc = "Telescope find files" })
-        vim.keymap.set('n', '<leader>pf', builtin.git_files, {}, { desc = "Telescope git files" })
-        vim.keymap.set('n', '<leader>pws', function()
+        vim.keymap.set("n", "<C-p>", builtin.find_files, {}, { desc = "Telescope find files" })
+        vim.keymap.set("n", "<leader>pf", builtin.git_files, {}, { desc = "Telescope git files" })
+
+        vim.keymap.set("n", "<leader>pws", function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end, { desc = "Telescope grep cword" })
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
+
+        vim.keymap.set("n", "<leader>pWs", function()
+            local word  vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end, { desc = "Telescope grep cWORD" })
-        vim.keymap.set('n', '<leader>ps', function()
+
+        vim.keymap.set("n", "<leader>ps", function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end, { desc = "Telescope grep string" })
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = "Telescope help tags" })
+
+        -- TODO: grep_string ({ search = <virtual> })
+
+        vim.keymap.set("n", "<leader>vh",
+            builtin.help_tags,
+       { desc = "Telescope help tags" })
+
+        -- TODO: change the following for a decent function
+        vim.keymap.set("n", "<leader>pp", ":Telescope<cr>", { desc = "Telescope in command line" })
+        vim.keymap.set("n", "<leader>pt", builtin.telescope, { desc = "Telescope in command line" })
+        vim.keymap.set("n", "<leader>pg", builtin.live_grep, { desc = "Telescope live grep" })
+        vim.keymap.set("n", "<leader>pcb", builtin.current_buffer_fuzzy_find, { desc = "Telescope current buffer fuzzy find" })
+        vim.keymap.set("n", "<leader>pm", builtin.marks, { desc = "Telescope marks" })
+        vim.keymap.set("n", "<leader>pr", builtin.registers, { desc = "Telescope registers" })
+        vim.keymap.set("n", "<leader>pk", builtin.keymaps, { desc = "Telescope keymaps" })
     end
 }
 
