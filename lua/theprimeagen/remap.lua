99a46c17aefda3e889d0caefced0e523958bbf7c
luan-brav0 <luan-brav0@github.com>
2025-05-01 00:31:02 -0300

    Minor: Mod: comment of 'J' map

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Explore" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 
-vim.keymap.set("n", "J", "mzJ`z", { desc = "Join line" })
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join line w/o moving cursor" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
