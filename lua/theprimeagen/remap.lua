fd4fd35940376caf89aea7baf0ef85f509320458
luan-brav0 <luan-brav0@github.com>
2025-05-01 22:06:21 -0300

    Minor: clipboard maps

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -23,11 +23,11 @@ end)
 -- greatest remap ever
 -- TODO: fix "conflict" with Telescope key maps
 vim.keymap.set("x", "<leader>dp", [["_dP]], { desc = "Delete to void and Paste" })
+vim.keymap.set({ "n", "v" }, "<leader>P", [["+p]], { desc = "Paste from clipboard" })
 
-vim.keymap.set({ "n", "v" }, "<leader>P", [["+p]], { desc = "Past from clipboard" })
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank selection to clipboard" })
-vim.keymap.set({ "n", "v" }, "<leader>Y", [["+Y]], { desc = "Yank line to clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>Y", [["+Y]], { desc = "Yank from cursor to end of  line to clipboard" })
 
 vim.keymap.set({ "n", "v" }, "<leader>D", [["_d]], { desc = "Delete to void" })
 vim.keymap.set({ "n", "v" }, "<leader>d", [["+d]], { desc = "Delete to clipboard" })
