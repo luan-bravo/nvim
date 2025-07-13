493ad438ba287a1552308cbae201099465ec49c9
luan-brav0 <luan-brav0@github.com>
2025-07-13 13:08:40 -0300

    change require error to send error notification instead of printing

--- lua/lul/init.lua
+++ lua/lul/init.lua
@@ -1,17 +1,28 @@
 local set_ok, _ = pcall(require, "lul.set")
-if not set_ok then print("[nvim]: Failed to require 'lul.set'") end
+if not set_ok then
+    vim.notify("Failed to require 'lul.set'", vim.log.levels.ERROR)
+end
 local remap_ok, _ = pcall(require, "lul.remap")
-if not remap_ok then print("[nvim]: Failed to require 'lul.remap'") end
+if not remap_ok then
+    vim.notify("Failed to require 'lul.remap'", vim.log.levels.ERROR)
+end
 local lazy_ok, _ = pcall(require, "lul.lazy")
-if not lazy_ok then print("[nvim]: Failed to require 'lul.lazy'") end
+if not lazy_ok then
+    vim.notify("Failed to require 'lul.lazy'", vim.log.levels.ERROR)
+end
 local functions_ok, _ = pcall(require, "lul.functions")
-if not functions_ok then print("[nvim]: Failed to require 'lul.functions'") end
+if not functions_ok then
+    vim.notify("Failed to require 'lul.functions'", vim.log.levels.ERROR)
+end
 local autocmd_ok, _ = pcall(require, "lul.autocmd")
-if not autocmd_ok then print("[nvim]: Failed to require 'lul.autocmd'") end
+if not autocmd_ok then
+    vim.notify("Failed to require 'lul.autocmd'", vim.log.levels.ERROR)
+end
 local clipboard_ok, _ = pcall(require, "lul.clipboard")
-if not clipboard_ok then print("[nvim]: Failed to require 'lul.clipboard'") end
+if not clipboard_ok then
+    vim.notify("Failed to require 'lul.clipboard'", vim.log.levels.ERROR)
+end
 
--- (?)
 function R(name)
     local r_status_ok, reload = pcall(require, "plenary.reload")
     if not r_status_ok then return end
