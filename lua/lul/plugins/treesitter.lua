b28433b1f35dda9c4cd68fe5531aeaf6d66e3ac1
luan-brav0 <luan-brav0@github.com>
2025-07-13 13:10:21 -0300

    remove comments and move markdown en. ins.

--- lua/lul/plugins/treesitter.lua
+++ lua/lul/plugins/treesitter.lua
@@ -7,35 +7,25 @@ return {
         ts_configs.setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "c", "cpp", "lua", "rust", "zig", "markdown", "go", "java", "kotlin", "python",
+                "c", "cpp", "lua", "rust", "zig", "go", "java", "kotlin", "python",
                 "javascript", "typescript", "css", "html",
                 "bash", "nix",
                 "toml", "yaml", "json", "xml",
+                "markdown", "markdown_inline", "regex",
                 "vimdoc", "jsdoc",
             },
             ignore_install = {
-                "gitcommit", -- It removes git commit diff highlighting for some reason
+                "gitcommit",
             },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
             auto_install = true,
-
             indent = {
                 enable = true
             },
 
             highlight = {
-                -- `false` will disable the whole extension
                 enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = { "markdown" },
             },
         })
