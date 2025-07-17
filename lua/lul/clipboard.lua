8239d8f57996f15eacd326f09858e839e4af80aa
luan-brav0 <luan-brav0@github.com>
2025-07-17 17:42:33 -0300

    correct cliphist commands | paste still not perfect

--- lua/lul/clipboard.lua
+++ lua/lul/clipboard.lua
@@ -3,6 +3,7 @@ local is_wsl = vim.fn.has("wsl") == 1 or os.getenv("WSL_DISTRO_NAME") ~= nil
 local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
 local is_linux = vim.fn.has("unix") == 1 and not (is_wsl or is_windows)
 if is_wsl or is_windows then
+
     vim.g.clipboard = {
         name = "wslclipboard",
         copy = {
@@ -31,15 +32,24 @@ elseif is_linux then
         }
     elseif vim.fn.executable("cliphist") == 1 then
         vim.g.clipboard = {
-            name = "cliphist",
+            -- name = "cliphist",
+            name = "wl-clipboard-cliphist",
             copy = {
-                ["+"] = "cliphist copy",
-                ["*"] = "cliphist copy",
+                ["+"] = "cliphist store",
+                ["*"] = "cliphist store",
             },
             paste = {
-                ["+"] = "cliphist decode | head -n 1",
-                ["*"] = "cliphist decode | head -n 1",
+                ["+"] = "wl-paste --no-newline",
+                ["*"] = "wl-paste --primary --no-newline",
             },
+            -- paste = {
+            --     ["+"] = "cliphist list | head -n 1 | cliphist decode",
+            --     ["*"] = "cliphist list | head -n 1 | cliphist decode",
+            -- },
+            -- copy = {
+            --     ["+"] = "wl-copy --foreground --type text/plain",
+            --     ["*"] = "wl-copy --foreground --primary --type text/plain",
+            -- },
             cache_enabled = 0,
         }
     else
@@ -51,3 +61,4 @@ elseif is_linux then
         }
     end
 end
+print("Current Clipboard: " .. vim.g.clipboard.name)
