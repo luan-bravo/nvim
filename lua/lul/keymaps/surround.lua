196938ad08b29b3eea26ed5027b1fdd5e79f379d
luan-brav0 <luan-brav0@github.com>
2025-09-02 01:50:05 -0300

    refactor(surround): make surround use an array of arguments to be served through a loop to a function to generate the keymaps

--- lua/lul/keymaps/surround.lua
+++ lua/lul/keymaps/surround.lua
@@ -1,28 +1,68 @@
 -------------------
 -- Surround Text --
 -------------------
+-- first second third
+vim.keymap.set({ "n","v" }, "s", "<nop>")
 vim.keymap.set({ "n","v" }, "S", "<nop>")
-vim.keymap.set("v", "S\"", "di\"\"<esc>P", { desc = "Surround w/ \"\"" })
-vim.keymap.set("v", "Sq", "di\"\"<esc>P", { desc = "Surround w/ \"\"" })
-vim.keymap.set("v", "S'", "di''<esc>P", { desc = "Surround w/ ''" })
-vim.keymap.set("v", "SQ", "di''<esc>P", { desc = "Surround w/ ''" })
-vim.keymap.set("v", "S`", "di``<esc>P", { desc = "Surround w/ ``" })
-vim.keymap.set("v", "SB", "di``<esc>P", { desc = "Surround w/ ``" })
-vim.keymap.set("v", "S<", "di<><esc>P", { desc = "Surround w/ <>" })
-vim.keymap.set("v", "St", "di<><esc>P", { desc = "Surround w/ <>" })
-vim.keymap.set("v", "S>", "di</><esc>P", { desc = "Surround w/ </>" })
-vim.keymap.set("v", "ST", "di</><esc>P", { desc = "Surround w/ </>" })
-vim.keymap.set("v", "S{ ", "di{}<esc>P", { desc = "Surround w/ { }" })
-vim.keymap.set("v", "S<leader>{", "di{  }<esc>hP", { desc = "Surround w/ {  }" })
-vim.keymap.set("v", "S(", "di()<esc>P", { desc = "Surround w/ ()" })
-vim.keymap.set("v", "S[", "di[]<esc>P", { desc = "Surround w/ []" })
-vim.keymap.set("v", "S<leader>[", "di[  ]<esc>hP", { desc = "Surround w/ [  ]" })
-vim.keymap.set("v", "Sl", "di[[]]<esc>hP", { desc = "Surround w/ [[]]" })
-vim.keymap.set("v", "S<leader>l", "di[[  ]]<esc>hhP", { desc = "Surround w/ [[  ]]" })
-vim.keymap.set("v", "S<leader>L", "di[[  ]];<esc>hhP", { desc = "Surround w/ [[  ]];" })
-vim.keymap.set("v", "S*", "di**<esc>P", { desc = "Surround w/ **" })
-vim.keymap.set("v", "S_", "di__<esc>P", { desc = "Surround w/ __" })
-vim.keymap.set("v", "Si", "di__<esc>P", { desc = "Surround w/ __" })
-vim.keymap.set("v", "Sb", "di****<esc>hP", { desc = "Surround w/ ****" })
-vim.keymap.set("v", "SI", "di_****_<esc>hhP", { desc = "Surround w/ _****_" })
-vim.keymap.set("v", "SB", "di_****_<esc>hhP", { desc = "Surround w/ _****_" })
+
+local pairs = {
+    {"\"\"", "\""},
+    {"\"\"", "q"},
+    {"''", "'"},
+    {"''", "Q"},
+    {"``", "`"},
+    {"``", "c"},
+
+    {"<>", "<"},
+    {"<>", "t"},
+    {"</>", ">"},
+    {"</>", "T"},
+
+    {"{}", "{"},
+    {"{  }", "<space>{"},
+
+    {"()", "("},
+
+    {"[]", "["},
+    {"[[]]", "l"},
+    {"[  ]", "<space>["},
+    {"[[  ]]", "<space>l"},
+    {"[[  ]];", "<space>L"},
+
+    {"**", "*"},
+    {"__", "_"},
+    {"__", "i"},
+    {"****", "b"},
+    {"_****_", "I"},
+    {"_****_", "B"},
+}
+
+for _, p in ipairs(pairs) do
+    local pair = p[1]; local trigger = p[2]; local padding = p[3]
+    -- print(pair .. " " .. trigger .. " ")
+
+
+    if (trigger == "") then trigger = pair end
+
+    -- Padding for inserting to be surrounded text centralized to the surrounding text
+    local pad = ""
+    if (padding == nil) then padding = (#pair/2)-1 end
+    pad = string.rep("h", padding)
+
+    -- print(pair .. " " .. trigger .. " " .. pad)
+
+
+    local desc = "Surround w/ " .. pair
+
+    local vPattern = "c" .. pair .. "<Esc>" .. pad .. "P"
+
+    local nwPattern = "viwc" .. pair .. "<Esc>" .. pad .. "P"
+    local nWPattern = "viWc" .. pair .. "<Esc>" .. pad .. "P"
+
+
+    vim.keymap.set("v", "s" .. trigger, vPattern, { desc = desc})
+    vim.keymap.set("v", "S" .. trigger, vPattern, { desc = desc})
+
+    vim.keymap.set("n", "s" .. trigger, nwPattern, { desc = desc})
+    vim.keymap.set("n", "S" .. trigger, nWPattern, { desc = desc})
+end
