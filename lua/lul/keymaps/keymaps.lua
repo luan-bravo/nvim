16d5cc38b8c3818f3a83c5bb3c8c522df0f2b7e6
luan-brav0 <luan-brav0@github.com>
2025-09-07 23:39:08 -0300

    Refactor: (keymaps.keymaps): Require keymaps iteratively

---
+++ lua/lul/keymaps/keymaps.lua
@@ -0,0 +1,21 @@
+local keymaps = {
+    "basics_and_moves",
+    "great_yet_simple",
+    "files_and_buffers",
+    "functions",
+    "formatting",
+    "substitute",
+    "surround",
+    "misc",
+    "clipboard",
+}
+
+for _, m in ipairs(keymaps) do
+    local mod_path = "lul.keymaps." .. m
+
+    local ok, _mod = pcall(require, mod_path)
+
+    if not ok then
+        vim.notify("Failed to require '" .. mod_path .. "'", vim.log.levels.ERROR)
+    end
+end
