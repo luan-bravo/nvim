4d2a061f61a7e52ed72efb88817b56b99881067b
luan-brav0 <luan-brav0@github.com>
2025-09-17 15:57:26 -0300

    refacto (ai.lua): Remove files for maven and gemini in favor of one generic 'ai.lua' plugins config file

---
+++ lua/lul/plugins/ai.lua
@@ -0,0 +1,51 @@
+return {
+    -- {
+    -- "marcinjahn/gemini-cli.nvim",
+    -- cmd = "Gemini",
+    -- -- Example key mappings for common actions:
+    -- keys = {
+    --     { "<leader>a/", "<cmd>Gemini toggle<cr>", desc = "Toggle Gemini CLI" },
+    --     { "<leader>aa", "<cmd>Gemini ask<cr>", desc = "Ask Gemini", mode = { "n", "v" } },
+    --     { "<leader>af", "<cmd>Gemini add_file<cr>", desc = "Add File" },
+    --
+    -- },
+    -- dependencies = {
+    --     "folke/snacks.nvim",
+    -- },
+    -- config = function()
+    --     require("gemini-cli").setup({
+    --         tab = true,
+    --     })
+    -- end,
+    --
+    -- },
+    -- {
+    --     "supermaven-inc/supermaven-nvim",
+    --     config = function()
+    --         local sm_ok, sm = pcall(require, "supermaven-nvim")
+    --         if not sm_ok then return end
+    --         sm.setup({
+    --             -- make maven only suggest when asked
+    --             keymaps = {
+    --                 accept_suggestion = "<Tab>",
+    --                 clear_suggestion = "<C-]>",
+    --                 accept_word = "<C-j>",
+    --             },
+    --
+    --
+    --             -- ignore_filetypes = { cpp = true }, -- or { "cpp", }
+    --             color = {
+    --                 suggestion_color = "#b16286",
+    --                 cterm = 244,
+    --             },
+    --             log_level = "info",
+    --             disable_inline_completion = false,
+    --             disable_keymaps = false,
+    --             -- Condition to check for stopping supermaven, `true` to stop
+    --             condition = function()
+    --                 return true
+    --             end
+    --         })
+    --     end
+    -- },
+}
