f331bf05b183b10be9dd82d1acbc1a0f72ab5ff5
luan-brav0 <luan-brav0@github.com>
2025-10-01 00:29:16 -0300

    feat (listchars): Add ' ' for tabs

--- lua/lul/options.lua
+++ lua/lul/options.lua
@@ -36,7 +36,7 @@ vim.opt.updatetime = 50
 
 vim.opt.list = true
 
-vim.opt.listchars:append("tab:  ")
+vim.opt.listchars:append("tab: ")
 vim.opt.listchars:append("trail:▒")
 
 vim.g.netrw_browse_split = 0
