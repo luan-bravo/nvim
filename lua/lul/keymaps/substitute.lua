822a03efbd67cb16b43bdc3f7c0b8e428ac19158
luan-brav0 <luan-brav0@github.com>
2025-10-15 23:32:34 -0300

    feat(substitute): easier substitute cword and cWORD in normal mode

--- lua/lul/keymaps/substitute.lua
+++ lua/lul/keymaps/substitute.lua
@@ -1,5 +1,9 @@
 --------------
 -- Easy SEd --
 --------------
-vim.keymap.set("v", "<leader>r", [[y:s/<C-r>"//g<Left><Left>]], { desc = "Replace selection in lines", noremap = true })
-vim.keymap.set("v", "<leader>R", [[y:%s/<C-r>"//g<Left><Left>]], { desc = "Replace selection in file", noremap = true })
+
+vim.keymap.set("v", "<leader>r", [[y:%s/\C<C-r>"//gcI<Left><Left><Left><Left>]], { desc = "Replace selection in file" })
+vim.keymap.set("v", "<leader>R", [[y:%s/\C<C-r>"//gI<Left><Left><Left>]], { desc = "Replace selection in line" })
+
+vim.keymap.set("n", "<leader>r", [[viwy:%s/\C\<<C-r>"\>//gcI<Left><Left><Left><Left>]], { desc = "Replace <cWORD> in file" })
+vim.keymap.set("n", "<leader>R", [[viWy:%s/\C<C-r>"//gI<Left><Left><Left>]], { desc = "Replace <cword> in line" })
