54cbb645cd60365d4775010695ec5a8bbb254708
luan-brav0 <luan-brav0@github.com>
2025-10-31 01:25:08 -0300

    tweak(options): Turn hlsearch on

--- lua/lul/options.lua
+++ lua/lul/options.lua
@@ -31,7 +31,7 @@ vim.opt.swapfile = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
